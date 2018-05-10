9ce862c33d0173e47bb56c2a4c80d87c6a2351d3
phhaaz <phhaanz@naver.com>
2018-05-10 17:49:11 +0900

    [phz]언제완성할까

--- nwprj/src/main/java/com/notework/nw/entity/view/TagView.java
+++ nwprj/src/main/java/com/notework/nw/entity/view/TagView.java
@@ -4,15 +4,22 @@ import com.notework.nw.entity.Tag;
 
 public class TagView extends Tag {
 
+	private int memberId;
 	private int noteCnt;
-
-	public TagView(String id, int noteCnt) {
+	
+	public TagView(String id, int memberId, int noteCnt) {
 		super(id);
 		this.noteCnt = noteCnt;
 	}
 	public TagView() {
 
 	}
+	public int getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(int memberId) {
+		this.memberId = memberId;
+	}	
 	public int getNoteCnt() {
 		return noteCnt;
 	}
